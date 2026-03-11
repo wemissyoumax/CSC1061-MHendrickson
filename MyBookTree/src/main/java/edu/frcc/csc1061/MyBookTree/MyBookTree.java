7cda071ab66a97c9e9d6081eeb04161295a962dd
Max Hendrickson <maxwellhendrickson11@gmail.com>
2026-03-11 14:51:56 -0600

    SearchEngine

--- MyBookTree/src/main/java/edu/frcc/csc1061/MyBookTree/MyBookTree.java
+++ MyBookTree/src/main/java/edu/frcc/csc1061/MyBookTree/MyBookTree.java
@@ -51,8 +51,43 @@ public class MyBookTree implements Iterable<MyBookNode>{
 
 	@Override
 	public Iterator<MyBookNode> iterator() {
-		return new MyBookIterator();
+		return new RecursiveIterator();
 	}
+	
+	private class RecursiveIterator implements Iterator<MyBookNode> {
+		Deque<MyBookNode> queue = new ArrayDeque<>();
+		
+		public RecursiveIterator() {
+			preorder(root);
+		}
+		//As it sees the nodes, it throws them in the queue
+		// ------------------------------------------------
+		// This block goes through the tree and sees who has children or not, preorderly
+		// (Ex: A, B, D, E, C, F, G)
+		// B has two children, E & F, and C has two children, F & G
+		// The loop goes through and if E doesn't have children, it "pops" back up to B to find F
+		private void preorder(MyBookNode node) {
+			queue.add(node);
+			if(node.getChildNodes() == null
+					|| node.getChildNodes().isEmpty()) {
+				return;
+			}
+			for(MyBookNode child : node.getChildNodes()) {
+				preorder(child);
+			}
+		}
+		@Override
+		public boolean hasNext() {
+			return !queue.isEmpty();
+		}
+		//This is our remove function
+		//Removes the first item in the queue and moves on to the next in queue
+		@Override
+		public MyBookNode next() {
+			return queue.removeFirst();
+		}
+	}
+	
 	private class MyBookIterator implements Iterator<MyBookNode> {
 		Deque<MyBookNode> stack;
 		public MyBookIterator() {
