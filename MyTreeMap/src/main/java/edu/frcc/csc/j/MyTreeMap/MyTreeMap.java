8a6f6bb9b38a712974e274a7c94650bd196c5de7
Hendrickson <S02508045@ccc.ccofc.edu>
2026-03-27 14:55:57 -0600

    MyTreeMap

--- MyTreeMap/src/main/java/edu/frcc/csc/j/MyTreeMap/MyTreeMap.java
+++ MyTreeMap/src/main/java/edu/frcc/csc/j/MyTreeMap/MyTreeMap.java
@@ -1,10 +1,15 @@
 package edu.frcc.csc.j.MyTreeMap;
 
+import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class MyTreeMap<K extends Comparable<K>,V> implements Map<K,V>{
+public class MyTreeMap<K extends Comparable<K>,V> implements Map<K,V>, Iterable<V>{
 	private Node root = null;
 	private int size = 0;
 	
@@ -13,11 +18,11 @@ public class MyTreeMap<K extends Comparable<K>,V> implements Map<K,V>{
 		protected V value;
 		protected Node lChild;
 		protected Node rChild;
-	}
 	
-	public Node (K key, V value) {
-		this.key = key;
-		this.value = value;
+		public Node (K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
 	}
 
 	@Override
@@ -44,7 +49,17 @@ public class MyTreeMap<K extends Comparable<K>,V> implements Map<K,V>{
 
 	@Override
 	public V get(Object key) {
-		// TODO Auto-generated method stub
+		Node current = root;
+		Comparable<K> k = (Comparable<K>) key;
+		while(current != null) {
+			if(k.compareTo(current.key)< 0) {
+				current = current.lChild;
+			} else if (k.compareTo(current.key)> 0) {
+				current = current.rChild;
+			} else {
+				return current.value;
+			}
+		}
 		return null;
 	}
 
@@ -81,8 +96,64 @@ public class MyTreeMap<K extends Comparable<K>,V> implements Map<K,V>{
 
 	@Override
 	public V remove(Object key) {
-		// TODO Auto-generated method stub
-		return null;
+		Comparable<K> k = (Comparable<K>) key;
+		V value = null;
+		//Find node first
+		Node current = root;
+		Node parent = null;
+		while(current != null) {
+			if(k.compareTo(current.key)< 0) {
+				parent = current;
+				current = current.lChild;
+			} else if (k.compareTo(current.key) > 0) {
+				parent = current;
+				current = current.rChild;
+			} else {
+				value = current.value;
+				break;
+			}
+		}
+		//Case 1 - no children
+		if(current.lChild == null && current.rChild == null) {
+			if(current == parent.lChild) {
+				parent.lChild = null;
+			} else {
+				parent.rChild = null;
+			}
+		}
+		//Case 3 (build 2 last) - 1 child
+		else if (current.lChild == null) {
+			if(current == parent.lChild) {
+				parent.lChild = current.rChild;
+			} else {
+				parent.rChild = current.rChild;
+			}
+		}
+		else if (current.lChild == null) {
+			if(current == parent.lChild) {
+				parent.lChild = current.lChild;
+			} else {
+				parent.rChild = current.lChild;
+			}
+		}
+		//Case 2
+		if(current.lChild != null && current.rChild != null) {
+			Node inOrderPred = current.lChild;
+			Node parentOfInOrder = current;
+			while(inOrderPred.rChild != null) {
+				parentOfInOrder = inOrderPred;
+				inOrderPred = inOrderPred.rChild;
+			}
+			current.key = inOrderPred.key;
+			current.value = inOrderPred.value;
+			
+			if(parentOfInOrder == current) {
+				current.lChild = inOrderPred;
+			} else {
+				parentOfInOrder.rChild = null;
+			}
+		}
+		return value;
 	}
 
 	@Override
@@ -93,20 +164,24 @@ public class MyTreeMap<K extends Comparable<K>,V> implements Map<K,V>{
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		root = null;
+		size = 0;
 	}
 
 	@Override
 	public Set<K> keySet() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Collection<V> values() {
-		// TODO Auto-generated method stub
-		return null;
+		List<V> values = new LinkedList<>();
+		Iterator<V> it = iterator();
+		while (it.hasNext()) {
+			values.add(it.next());
+		}
+		return values;
 	}
 
 	@Override
@@ -114,7 +189,80 @@ public class MyTreeMap<K extends Comparable<K>,V> implements Map<K,V>{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
+	@Override
+	public Iterator<V> iterator() {
+		return new NonRecursiveIterator();
+	}
+	private class NonRecursiveIterator implements Iterator<V> {
+		private Deque<Node> stack = new ArrayDeque<>();
+		
+		public NonRecursiveIterator() {
+			pushOnStack(root);
+		}
+		
+		private void pushOnStack(Node node) {
+			Node current = node;
+			while (current != null) {
+				stack.push(current);
+				current = current.lChild;
+			}
+		}
+
+		@Override
+		public boolean hasNext() {
+			return !stack.isEmpty();
+		}
+
+		@Override
+		public V next() {
+			Node node = stack.pop();
+			pushOnStack(node.rChild);
+			return node.value;
+		}
+	}
+	private class RecursiveIterator implements Iterator<V> {
+		
+		private List<V> list = new LinkedList<>();
+		
+		public RecursiveIterator() {
+			inOrder(root);
+		}
+		
+		private void inOrder(Node node) {
+			if(node == null) {
+				return;
+			}
+			
+			inOrder(node.lChild);
+			list.add(node.value);
+			inOrder(node.rChild);
+		}
+		
+		private void preOrder(Node node) {
+			if(node == null) {
+				return;
+			}
+			list.add(node.value);
+			preOrder(node.lChild);
+			preOrder(node.rChild);
+		}
+		
+		private void postOrder(Node node) {
+			
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return !list.isEmpty();
+		}
+
+		@Override
+		public V next() {
+			return list.remove(0);
+		}
+		
+	}
 	
 }
 	
