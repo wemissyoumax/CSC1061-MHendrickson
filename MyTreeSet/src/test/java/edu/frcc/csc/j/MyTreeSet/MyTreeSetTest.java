7491c4e83ef1dde47b37f815d79a09e60b8f5db8
Maxwell Hendrickson <maxwellhendricksongit config --global user.email maxwellhendrickson>
2026-04-01 14:53:35 -0600

    MyTreeSet

---
+++ MyTreeSet/src/test/java/edu/frcc/csc/j/MyTreeSet/MyTreeSetTest.java
@@ -0,0 +1,17 @@
+package edu.frcc.csc.j.MyTreeSet;
+
+public class MyTreeSetTest {
+	MyTreeSet<Integer> set = new MyTreeSet<>();
+	
+	set.add(1);
+	set.add(2);
+	set.add(3);
+	set.add(4);
+	set.add(5);
+	set.add(6);
+	set.add(7);
+	set.add(8);
+	set.add(9);
+	set.add(10);
+	set.add(11);
+}
