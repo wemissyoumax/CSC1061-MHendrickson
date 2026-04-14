b1a02eb8fc5cefff21bcd24e99e9f50b3c6c56fa
Maxwell Hendrickson <maxwellhendricksongit config --global user.email maxwellhendrickson>
2026-04-14 17:26:13 -0600

    Exam2

--- Exam2/src/main/java/edu/frcc/csc/j/Exam2/FileTree.java
+++ Exam2/src/main/java/edu/frcc/csc/j/Exam2/FileTree.java
@@ -1,102 +1,96 @@
 package edu.frcc.csc.j.Exam2;
-
 import java.io.File;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.Queue;
+import java.util.LinkedList;
 
+public class FileTree implements Iterable<FileNode> {
+    private FileNode root;
 
+    public FileTree(String path) {
+        root = new FileNode(path);
+        buildTree(root);
+    }
 
-public class FileTree implements Iterable <FileNode> {
+    @Override
+    public Iterator<FileNode> iterator() {
+        return new DepthFirstIterator();
+    }
 
-	private FileNode root;
-	
-	public FileTree(String path) {
-		root = new FileNode(path);
-		buildTree(root);
-	}
+    // TODO 1: buildTree
+    private void buildTree(FileNode fileNode) {
+        File file = fileNode.getFile();
 
-	/**
-	 * Return a depth first post-order traversal iterator 
-	 */
-	@Override
-	public Iterator<FileNode> iterator() {
+        if (file.isDirectory()) {
+            File[] children = file.listFiles();
+            if (children != null) {
+                for (File child : children) {
+                    FileNode childNode = new FileNode(child);
+                    fileNode.getChildNodes().add(childNode);
+                    buildTree(childNode);
+                    fileNode.setFileSize(fileNode.getFileSize() + childNode.getFileSize());
+                }
+            }
+        }
+    }
 
-		return new DepthFirstIterator();
-	}
-	
-	/**
-	 * 	TODO for Exam 2
-	 *  Use recursion to build the tree from the directory structure.
-	 *  For each of node starting from the root node use listFiles() from File to get 
-	 *  the list of files in that directory/folder.
-	 *  Create a node for each of the files and add it to a list of child nodes for the node
-	 *  Do this recursively for all the nodes.  
-	 * 
-	 * @param fileNode
-	 */
-	private void buildTree(FileNode fileNode) {
+    // TODO 2: DepthFirstIterator (post-order, two-stack approach)
+    private class DepthFirstIterator implements Iterator<FileNode> {
 
-	
-	}
-	
-	/**
-	 * TODO for Exam 2
-	 * Iterator that does a post order traversal of the tree.
-	 * For post-order traversal use the 2 stack approach outlined here: 
-	 * https://www.geeksforgeeks.org/iterative-postorder-traversal/
-	 * 
-	 * @return 
-	 */
-	private class DepthFirstIterator implements Iterator<FileNode> {
-		
-		public DepthFirstIterator() {
+        private Deque<FileNode> resultStack = new ArrayDeque<>();
 
-		}
+        public DepthFirstIterator() {
+            Deque<FileNode> stack = new ArrayDeque<>();
+            stack.push(root);
 
-		@Override
-		public boolean hasNext() {
-			return true;
-		}
-		
-		@Override
-		public FileNode next() {
-			return null;
-		}
-	}
-	
-	/**
-	 *  Returns an iterator that does a breadth first traversal of the tree using a queue.
-	 * 
-	 * @return
-	 */
-	public Iterator<FileNode> breadthFirstIterator() {
-		
-		return new BreadthFirstIterator();
+            while (!stack.isEmpty()) {
+                FileNode node = stack.pop();
+                resultStack.push(node);
 
-	}	
-	
-	/** 
-	 * TODO for Exam 2
-	 * Iterator that does a breadth first traversal of the tree using a queue.
-	 * 
-	 */
-	private class BreadthFirstIterator implements Iterator<FileNode> {
-		
-		public BreadthFirstIterator() {
+                for (FileNode child : node.getChildNodes()) {
+                    stack.push(child);
+                }
+            }
+        }
 
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return true;
-		}
+        @Override
+        public boolean hasNext() {
+            return !resultStack.isEmpty();
+        }
 
-		@Override
-		public FileNode next() {
-			return null;
-		}
-		
-	}
+        @Override
+        public FileNode next() {
+            return resultStack.pop();
+        }
+    }
+
+    public Iterator<FileNode> breadthFirstIterator() {
+        return new BreadthFirstIterator();
+    }
+    
+    // TODO 3: BreadthFirstIterator (queue-based BFS)
+    private class BreadthFirstIterator implements Iterator<FileNode> {
+
+        private Queue<FileNode> queue = new LinkedList<>();
+
+        public BreadthFirstIterator() {
+            queue.offer(root);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !queue.isEmpty();
+        }
+
+        @Override
+        public FileNode next() {
+            FileNode node = queue.poll();
+            for (FileNode child : node.getChildNodes()) {
+                queue.offer(child);
+            }
+            return node;
+        }
+    }
 }
